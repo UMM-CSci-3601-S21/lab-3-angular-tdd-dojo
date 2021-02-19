20daf74405a529975b3212aa18960c66caa41c3e
Nic McPhee <mcphee@morris.umn.edu>
2021-02-18 18:09:46 -0600

    Get the TodoListComponent unit tests to pass
    I created a `MockTodoService` and set up a pretty minimal framework to get the `TodoListComponent` tests to pass.

--- client/src/app/todos/todo-list.component.spec.ts
+++ client/src/app/todos/todo-list.component.spec.ts
@@ -1,25 +1,26 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TodoListComponent } from './todo-list.component';
+import { TodoService } from './todo.service';
+import { MockTodoService } from '../../testing/todo.service.mock';
 
 describe('TodoListComponent', () => {
-  let component: TodoListComponent;
+  let todoList: TodoListComponent;
+  let todoService: TodoService;
   let fixture: ComponentFixture<TodoListComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ TodoListComponent ]
-    })
-    .compileComponents();
-  });
-
   beforeEach(() => {
-    fixture = TestBed.createComponent(TodoListComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    TestBed.configureTestingModule({
+      providers: [
+        TodoListComponent,
+        { provide: TodoService, useClass: MockTodoService }
+      ]
+    });
+    todoList = TestBed.inject(TodoListComponent);
+    todoService = TestBed.inject(TodoService);
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(todoList).toBeTruthy();
   });
 });
